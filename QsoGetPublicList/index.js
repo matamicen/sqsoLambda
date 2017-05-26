7c6bb1b2d467f0930008f73a6aace0e512468164
lisandro.langenauer <lisandro.langenauer@bairesdev.com>
2017-05-26 18:11:35 -0300

    QsoPublicList funcionando.

--- QsoGetPublicList/index.js
+++ QsoGetPublicList/index.js
@@ -8,7 +8,7 @@ exports.handler = (event, context, callback) => {
 
     context.callbackWaitsForEmptyEventLoop = false;
 
-    var msg;
+
 
 
     //***********************************************************
@@ -21,10 +21,15 @@ exports.handler = (event, context, callback) => {
 
 
     var date = new Date();
+    var qso_qras;
+    var qso_medias;
+    var qsos = [];
+    var msg;
+    var idqsos;
     console.log(date);
 
     console.log("Get QSOS up to today" + date);
-    conn.query ( "SELECT * from qsos WHERE datetime <=? LIMIT 100", date,   function(error,qsos) {
+    conn.query ( "SELECT * from qsos WHERE datetime <=? LIMIT 100", date,   function(error,rows) {
         if (error) {
             console.log("ERROR In Get QSOS up to today" + date);
             console.log(error);
@@ -34,28 +39,63 @@ exports.handler = (event, context, callback) => {
                 "message": "ERROR In Get QSOS up to today" + date };
             return context.fail(msg);
         } else {
-            console.log(JSON.stringify(qsos));
-
-            // async.map lets us apply a function to a collection.
-            async.map(qsos, [{
-                function (qso, callback) {
-                    conn.query ( "SELECT * from qsos WHERE datetime <=? LIMIT 100", date,   function(error,qsos) {
-                        if (error) {
-                            console.log("ERROR In Get QSOS up to today" + date);
-                            console.log(error);
-                            conn.destroy();
-                            callback(error.message);
-                            msg = { "error": "1",
-                                "message": "ERROR In Get QSOS up to today" + date };
-                            return context.fail(msg);
-                        }
-                    });
-                }
-            },
+            // console.log(JSON.stringify(qsos));
+            qsos = JSON.parse(JSON.stringify(rows));
+            console.log(qsos);
+            async.mapSeries(qsos,
+                function(qso,callback1)
                 {
+                    console.log("each item" + JSON.stringify(qso));
+                    async.series([
+                            function(callback2) {
+                                idqsos = JSON.stringify(qso.idqsos);
+                                console.log("function1" + idqsos);
+                                conn.query ( "SELECT * from qsos_qras WHERE idqso = ?", idqsos,   function(error,qras) {
+                                    if (!error) {
+                                        console.log(qras);
+                                        qso_qras = JSON.parse(JSON.stringify(qras));
+                                        qso.qras = qso_qras;
+                                        // qsos.push(JSON.parse(JSON.stringify(qso)));
+                                        console.log(qso);
+                                        callback2();
+                                    }
 
+                                });
+
+                            },
+                            function(callback2) {
+                                idqsos = JSON.stringify(qso.idqsos);
+                                console.log("function2" + idqsos);
+                                conn.query ( "SELECT * from qsos_media WHERE idqso =? ", idqsos,   function(error,media) {
+                                    if (!error) {
+                                        qso_media = JSON.parse(JSON.stringify(media));
+                                        qso.media = qso_media;
+                                        //qsos.push(JSON.parse(JSON.stringify(qso)));
+                                        console.log(qso);
+                                        callback2();
+                                    }
+
+                                });
+
+                            } ],
+                        function(err){
+                            console.log("All tasks are done now");
+                            // doSomethingOnceAllAreDone();
+                            console.log(qso);
+                            callback1();
+
+                        });
+                },
+
+                // 3rd param is the function to call when everything's done
+                function(err){
+                    console.log("All tasks are done now");
+                    // doSomethingOnceAllAreDone();
+                    console.log(qsos);
+                    context.succeed(qsos);
                 }
-            ]);
+
+            );
         }
     });
 
