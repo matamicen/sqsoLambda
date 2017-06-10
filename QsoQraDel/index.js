f578bd309af17d6df5e6e5c316007ca4967d2a89
lisandro.langenauer <lisandro.langenauer@bairesdev.com>
2017-06-10 12:15:04 -0300

    qragetprofilepic

---
+++ QsoQraDel/index.js
@@ -0,0 +1,132 @@
+var fs = require('fs');
+var mysql = require('mysql');
+var async = require('async');
+
+exports.handler = (event, context, callback) => {
+
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('Received context:', JSON.stringify(context, null, 2));
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    var Sub;
+    var post;
+    var json;
+    var qso;
+    var qras;
+    var qra;
+    var msg;
+    // var count;
+    if (process.env.TEST) {
+        var test = {
+            "qso": "336",
+            "qras": [   "lw1ppp",
+                "lu2ach"
+            ]
+        };
+        qso = test.qso;
+        qras = test.qras;
+        Sub = '9970517e-ed39-4f0e-939e-930924dd7f73';
+    }
+    else {
+        qso = event.body.qso;
+        qras = event.body.qras;
+        Sub = event.context.sub;
+    }
+
+
+    //***********************************************************
+    var conn = mysql.createConnection({
+        host      :  'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com' ,  // give your RDS endpoint  here
+        user      :  'sqso' ,  // Enter your  MySQL username
+        password  :  'parquepatricios' ,  // Enter your  MySQL password
+        database  :  'sqso'    // Enter your  MySQL database name.
+    });
+
+    // GET QRA ID of OWNER
+    console.log("select QRA to get ID of Owner");
+    console.log(qso);
+    conn.query ( "SELECT qras.idcognito from qras inner join qsos on qras.idqras = qsos.idqra_owner where qsos.idqsos =? and qras.idcognito=?", [qso , Sub],   function(error,info) {
+        console.log("info" + JSON.stringify(info));
+        if (error) {
+            console.log("Error when select QRA to get ID of Owner");
+            console.log(error);
+            conn.destroy();
+            callback(error.message);
+            msg = { "error": "1",
+                "message": "Error when select QRA to get ID of Owner" };
+            return context.fail(msg);
+        } else if (info.lenght === 0){
+            console.log("Caller user is not the QSO Owner");
+            console.log('error select: ' + error);
+            callback(error);
+            msg = { "error": "1",
+                "message": "Error when select QRA to get ID of Owner" };
+            return context.fail(msg);
+        } else {
+
+            // 1st para in async.each() is the array of items
+            //***********************************************************
+            async.each(qras,
+                // 2nd param is the function that each item is passed to
+                function(qra, callback){
+                    console.log("GET QRA", qra.toUpperCase());
+                    //***********************************************************
+                    conn.query ( "SELECT * FROM qras where qra=? ", qra.toUpperCase(),   function(error,info) {  // querying the database
+                        console.log("info=",info);
+                        console.log(info.length);
+                        if (error) {
+                            console.log("Error When GET QRA");
+                            console.log('error select: ' + error);
+                            callback(error);
+                            return context.fail(error);
+                        }
+                        else if (info.length > 0) {
+                            console.log("found");
+                            json =  JSON.parse(JSON.stringify(info));
+                            idqras = json[0].idqras;
+                            post  = {"idqso": qso,
+                                "idqra": idqras};
+                            console.log("post" +  idqras + qso);
+                            //***********************************************************
+                            conn.query('DELETE FROM qsos_qras where idqso=? and idqra=?', [ qso, idqras ], function(error, info) {
+                                console.log("info" + JSON.stringify(info));
+                                if (error) {
+                                    console.log("Error when Delete QSO QRA");
+                                    console.log(error.message);
+                                    conn.destroy();
+                                    callback(error.message);
+                                    return context.fail(error);
+                                } else if (info.affectedRows > 0){
+                                    console.log("QSOQRA deleted", info.affectedRows);
+                                    // count++;
+                                    msg = { "error": "O",
+                                        "message": "QSOQRA deleted " + info.affectedRows };
+                                    return context.succeed(msg);
+                                } else {
+                                    console.log("Error when Delete QSO QRA");
+                                    // count++;
+                                    msg = { "error": "1",
+                                        "message": "Error when Delete QSO QRA" };
+                                    return context.fail(JSON.stringify(msg));
+                                }
+                            }); //End Insert
+                        } else {
+                            msg = { "error": "1",
+                                "message": "QRA not found" };
+                            return context.fail(JSON.stringify(msg));
+                        }//end if
+                    }); //End Select
+                },
+                //***********************************************************
+                // 3rd param is the function to call when everything's done
+                function(err){
+                    console.log("All tasks are done now");
+                    // doSomethingOnceAllAreDone();
+                    var msg = { "error": "0",
+                        "message": qras  };
+                    context.succeed(msg);
+                }
+            ); //end async
+        }});
+
+};
