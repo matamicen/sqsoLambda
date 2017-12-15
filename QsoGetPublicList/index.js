4dc98680339286a8323611369d0f9ac387e17135
lisandrolan <lisandrolan@gmail.com>
2017-12-15 19:11:04 -0300

    qsogetpublic list funcionando con stored procedures

--- QsoGetPublicList/index.js
+++ QsoGetPublicList/index.js
@@ -8,172 +8,58 @@ exports.handler = (event, context, callback) => {
 
     context.callbackWaitsForEmptyEventLoop = false;
 
-
-
-
     //***********************************************************
     var conn = mysql.createConnection({
-        host      :  'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com' ,  // give your RDS endpoint  here
-        user      :  'sqso' ,  // Enter your  MySQL username
-        password  :  'parquepatricios' ,  // Enter your  MySQL password
-        database  :  'sqso'    // Enter your  MySQL database name.
+        host: 'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com', // give your RDS endpoint  here
+        user: 'sqso', // Enter your  MySQL username
+        password: 'parquepatricios', // Enter your  MySQL password
+        database: 'sqso' // Enter your  MySQL database name.
     });
 
 
-    var date = new Date();
+
     var qso_qras;
-    var qso_medias;
+    var qso_media;
     var qso_comments;
     var qso_likes;
-    var qra;
     var qsos = [];
     var qsos_output = [];
     var msg;
-    var idqsos;
-    console.log(date);
 
-    console.log("Get QSOS up to today" + date);
-    conn.query ( "SELECT * from qsos WHERE datetime <=? order by datetime desc LIMIT 50", date,   function(error,rows) {
+
+
+    conn.query("CALL qsopubliclistget( )", function(error, info) {
         //    conn.query ( "SELECT * from qsos WHERE idqsos = 399 order by datetime desc LIMIT 1",   function(error,rows) {
         if (error) {
-            console.log("ERROR In Get QSOS up to today" + date);
+            console.log("ERROR In Get QSOS up to today");
             console.log(error);
             conn.destroy();
             callback(error.message);
-            msg = { "error": "1",
-                "message": "ERROR In Get QSOS up to today" + date };
+            msg = {
+                "error": "1",
+                "message": "ERROR In Get QSOS up to today"
+            };
             return context.fail(msg);
-        } else {
-            // console.log(JSON.stringify(qsos));
-            qsos = JSON.parse(JSON.stringify(rows));
-            console.log(qsos);
-            async.mapSeries(qsos,
-                function(qso,callback1)
-                {
-                    console.log("each item" + JSON.stringify(qso));
+        }
+        else {
+            qsos = JSON.parse(JSON.stringify(info))[0];
+            qso_qras = JSON.parse(JSON.stringify(info))[1];
+            qso_comments = JSON.parse(JSON.stringify(info))[2];
+            qso_likes = JSON.parse(JSON.stringify(info))[3];
+            qso_media = JSON.parse(JSON.stringify(info))[4];
 
-                    async.series([
-                            function(callback2) { //GET QRA of QSO_OWNER
-                                idqsos = JSON.stringify(qso.idqsos);
-                                console.log("qra_owner - " + idqsos);
-                                conn.query ( "SELECT qra, profilepic from qras WHERE idqras = ? LIMIT 1", qso.idqra_owner,   function(error,info) {
-                                    if (!error) {
-                                        //console.log(info);
-                                        qra = JSON.parse(JSON.stringify(info));
-                                        //console.log(qra[0].qra);
-                                        //console.log(qra[0].profilepic);
-                                        qso.qra = qra[0].qra;
-                                        qso.profilepic = qra[0].profilepic;
-                                        // qsos.push(JSON.parse(JSON.stringify(qso)));
-                                        console.log(qso);
-                                        callback2();
-                                    }else{
-                                        console.log(error);
-                                        callback2();
-                                    }
+            qsos_output = qsos.map(qso => {
+                qso.qras = qso_qras.filter(obj => obj.idqsos === qso.idqsos);
+            qso.comments = qso_comments.filter(obj => obj.idqsos === qso.idqsos);
+            qso.likes = qso_likes.filter(obj => obj.idqsos === qso.idqsos);
+            qso.media = qso_media.filter(obj => obj.idqsos === qso.idqsos);
+            return qso;
+        });
+            qsos_output = qsos_output.filter(obj => obj.media.length > 0);
+            console.log(qsos_output);
+            conn.destroy();
+            context.succeed(qsos_output);
 
-                                });
-
-                            },
-                            function(callback3) { //GET QRAS of QSO
-                                idqsos = JSON.stringify(qso.idqsos);
-                                console.log("qsos_qras" + idqsos);
-                                conn.query ( "SELECT qra, profilepic FROM sqso.qras where  idqras in ( SELECT idqra FROM sqso.qsos_qras where isOwner <> true and idqso = ? ) ", idqsos,   function(error,qras) {
-                                    if (!error) {
-                                        console.log(qras);
-                                        qso_qras = JSON.parse(JSON.stringify(qras));
-                                        qso.qras = qso_qras;
-                                        // qsos.push(JSON.parse(JSON.stringify(qso)));
-                                        //  console.log(qso);
-                                        callback3();
-                                    }else{
-                                        console.log(error);
-                                        callback3();
-                                    }
-
-                                });
-
-                            },
-                            function(callback4) { //GET MEDIA
-                                idqsos = JSON.stringify(qso.idqsos);
-                                console.log("function2" + idqsos);
-                                conn.query ( "SELECT * from qsos_media WHERE idqso =? ", idqsos,   function(error,media) {
-                                    if (!error) {
-                                        qso_media = JSON.parse(JSON.stringify(media));
-                                        qso.media = qso_media;
-                                        //qsos.push(JSON.parse(JSON.stringify(qso)));
-                                        //     console.log(qso);
-                                        callback4();
-                                    }else{
-                                        console.log(error);
-                                        callback4();
-                                    }
-
-                                });
-
-                            },
-                            function (callback5) { //GET LIKES
-                                idqsos = JSON.stringify(qso.idqsos);
-                                console.log("GET LIKES" + idqsos);
-                                conn.query("SELECT qra, profilepic FROM sqso.qras where  idqras in (SELECT idqra from qsos_likes WHERE idqso =? )", idqsos, function (error, likes) {
-                                    if (!error) {
-                                        console.log(likes);
-                                        qso_likes = JSON.parse(JSON.stringify(likes));
-                                        qso.likes = qso_likes;
-                                        //qsos.push(JSON.parse(JSON.stringify(qso)));
-                                        //     console.log(qso);
-                                        callback5();
-                                    }
-                                    else {
-                                        console.log(error);
-                                        callback5();
-                                    }
-
-                                });
-
-                            },
-                            function (callback5) { //GET Comments
-                                idqsos = JSON.stringify(qso.idqsos);
-                                console.log("GET Comments" + idqsos);
-                                conn.query("SELECT qsos_comments.*, qras.qra FROM qsos_comments inner join qras on qsos_comments.idqra = qras.idqras where  idqso=?", idqsos, function (error, comments) {
-                                    if (!error) {
-                                        console.log(comments);
-                                        qso_comments = JSON.parse(JSON.stringify(comments));
-                                        qso.comments = qso_comments;
-                                        //qsos.push(JSON.parse(JSON.stringify(qso)));
-                                        //     console.log(qso);
-                                        callback5();
-                                    }
-                                    else {
-                                        console.log(error);
-                                        callback5();
-                                    }
-
-                                });
-
-                            } ],
-                        function(err){
-                            console.log("All tasks are done now");
-                            // doSomethingOnceAllAreDone();
-                            //    console.log(qso);
-                            qsos_output.push(qso);
-                            callback1();
-
-                        });
-                },
-
-                // 3rd param is the function to call when everything's done
-                function(err){
-                    console.log("All tasks are done now");
-                    // doSomethingOnceAllAreDone();
-                    console.log(qsos_output);
-                    conn.destroy();
-                    context.succeed(qsos_output);
-                }
-
-            );
         }
     });
-
-
 };
