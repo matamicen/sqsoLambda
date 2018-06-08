8aaf1c2718bcce8cc689046a4c875d8f91898fc9
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-06-08 18:07:03 -0300

    nueva api qsoMediaDel

---
+++ QsoMediaDel/index.js
@@ -0,0 +1,114 @@
+
+var mysql = require('mysql');
+// var async = require('async');
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1';
+
+
+exports.handler = (event, context, callback) => {
+
+
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    var Sub;
+
+var qso;
+
+    
+
+ var idmedia;
+ 
+
+    var response = {
+        "message": "",
+        "error": ""
+    };
+
+    // var count;
+    if (process.env.TEST) {
+        var test = {  
+          "idmedia": 1892,
+          "qso":  1307
+        };
+         idmedia = test.idmedia;
+         qso = test.qso;
+    }
+    else {
+        qso = event.body.qso;
+        idmedia = event.body.idmedia;
+    }
+
+    if (process.env.TEST){
+        Sub = "ccd403e3-06ae-4996-bb70-3aacf32a86df";
+    }else if (event.context.sub){
+        Sub = event.context.sub;
+    }
+    console.log("sub =", Sub);
+
+
+    //***********************************************************
+    var conn = mysql.createConnection({
+        host      :  'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com' ,  // give your RDS endpoint  here
+        user      :  'sqso' ,  // Enter your  MySQL username
+        password  :  'parquepatricios' ,  // Enter your  MySQL password
+        database  :  'sqso'    // Enter your  MySQL database name.
+    });
+
+    // GET QRA ID of OWNER
+    console.log("select QRA to get ID of Owner");
+    console.log(qso);
+    conn.query ( "SELECT qras.idqras, qras.qra from qras inner join qsos on qras.idqras = qsos.idqra_owner where qsos.idqsos =? and qras.idcognito=?", [qso , Sub],   function(error,info) {
+        if (error) {
+            console.log("Error when select QRA to get ID of Owner");
+            console.log(error);
+            conn.destroy();
+            response.error = 400;
+            response.message = "Error: select QRA to get ID of Owner";
+            // return context.fail( "Error: select QRA to get ID of Owner");
+            return context.succeed(response);
+        }
+
+
+
+        if (info.length === 0){
+            console.log("Caller user is not the QSO Owner");
+            response.error = 400;
+            response.message = "Error: Caller user is not the QSO Owner";
+            conn.destroy();
+            //return context.fail( "Error: Caller user is not the QSO Owner");
+            return context.succeed(response);
+        }
+
+        //***********************************************************
+
+        conn.query('UPDATE qsos_media SET deleted=1 WHERE idqsos_media=?', [idmedia], function(error, info) {
+            if (error) {
+                console.log("Error when Insert QSO MEDIA");
+                console.log(error.message);
+                conn.destroy();
+                response.error = 400;
+                response.message = "Error when Insert QSO MEDIA";
+                //return context.fail( "Error when Insert QSO MEDIA");
+                return context.succeed(response);
+            } //End If
+            console.log(info);
+              if (info.affectedRows) {
+                console.log("Media Deleted");
+
+                
+                var msg = {
+                    "error": "0",
+                    "message": info.message
+                };
+                console.log(info.message);
+               
+
+
+                //***********************************************************
+                
+                context.succeed(msg);
+            } //ENDIF 
+
+        }); //End Insert
+    });
+};
