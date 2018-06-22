bca3d6349ab2b16de43ca4f68f112e6594bbabf7
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-06-22 16:58:40 -0300

    resolves #5

--- CognitoPostConfirmation/index.js
+++ CognitoPostConfirmation/index.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var mysql = require('mysql');
 
 /**
@@ -14,52 +13,68 @@ exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('Received context:', JSON.stringify(context, null, 2));
     var conn = mysql.createConnection({
-        host      :  'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com' ,  // give your RDS endpoint  here
-        user      :  'sqso' ,  // Enter your  MySQL username
-        password  :  'parquepatricios' ,  // Enter your  MySQL password
-        database  :  'sqso'    // Enter your  MySQL database name.
+        host: 'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com', // give your RDS endpoint  here
+        user: 'sqso', // Enter your  MySQL username
+        password: 'parquepatricios', // Enter your  MySQL password
+        database: 'sqso' // Enter your  MySQL database name.
     });
 
-    conn.connect(function(err) {    // connecting to database
+    conn.connect(function(err) { // connecting to database
         if (err) {
             console.log('error connecting: ' + err.stack);
             callback(err.stack);
         }
-        console.log('connected as id ' + conn.threadId);	 //console.log(conn);
+        console.log('connected as id ' + conn.threadId); //console.log(conn);
     });
     var Sub;
     var Name;
-    if (event.key1){
-        Sub = event.key1;
-    }else if (event.request.userAttributes.sub){
+    var birthdate;
+    var email;
+    if (event.sub) {
+        Sub = event.sub;
+    }
+    else if (event.request.userAttributes.sub) {
         Sub = event.request.userAttributes.sub;
     }
-
-    if (event.key2){
-        Name = event.key.toUpperCase();
-    }else if (event.userName){
+    if (event.email) {
+        email = event.email;
+    }
+    else {
+        email = event.request.userAttributes.email;
+    }
+    if (event.birthdate) {
+        birthdate = new Date(event.birthdate);
+    }
+    else {
+        birthdate = new Date(event.request.userAttributes.birthdate);
+    }
+    
+    if (event.userName) {
+        Name = event.userName.toUpperCase();
+    }
+    else if (event.userName) {
         Name = event.userName.toUpperCase();
     }
     console.log('Sub =', Sub);
     console.log('userName =', Name);
 
-    conn.query ( "SELECT * FROM qras where qra=? LIMIT 1", Name.toUpperCase(),   function(error,info) {  // querying the database
-        console.log("error=",error);
-        console.log("info=",info);
+    conn.query("SELECT * FROM qras where qra=? LIMIT 1", Name.toUpperCase(), function(error, info) { // querying the database
+        console.log("error=", error);
+        console.log("info=", info);
         if (error) {
             console.log('error select: ' + error);
             callback(error);
         }
         if (info.length === 0) {
             //QRA Not Found => INSERT
-            var post  = {"qra" : Name.toUpperCase(), "idcognito" : Sub, "url": "empty"};
+            var post = { "qra": Name.toUpperCase(), "email": email, "birthday": birthdate, "idcognito": Sub, "url": "empty" };
             console.log('POST' + post);
-            conn.query ( 'INSERT INTO qras SET ?', post,   function(error,info) {  // querying the database
+            conn.query('INSERT INTO qras SET ?', post, function(error, info) { // querying the database
                 if (error) {
                     console.log(error.message);
-                    if(error.errno==1062)
+                    if (error.errno == 1062)
                         console.log("already exists");
-                    context.done(null,event);
+                    context.done(null, event);
                     conn.destroy();
                     callback(error.message);
                 }
@@ -71,16 +86,16 @@ exports.handler = (event, context, callback) => {
                 }
             }); //end Insert
         }
-        else  {
+        else {
             //QRA FOUND => Update QRA with ID Cognito
             var qra = JSON.stringify(info);
-            var json =  JSON.parse(qra);
+            var json = JSON.parse(qra);
             var idqras = json[0].idqras;
-            console.log("idqras=",idqras);
-            conn.query ( 'UPDATE qras SET idcognito=? WHERE idqras=?', [ Sub , idqras.toUpperCase() ],   function(error,info) {  // querying the database
+            console.log("idqras=", idqras);
+            conn.query('UPDATE qras SET birthday=?, email=?, idcognito=? WHERE idqras=?', [birthdate, email, Sub, idqras], function(error, info) { // querying the database
                 if (error) {
                     console.log(error.message);
-                    context.done(null,event);
+                    context.done(null, event);
                     conn.destroy();
                     callback(error.message);
                 }
