291a755a3e4ccb727578e619b4c81cc72ab636f4
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-06-22 12:24:31 -0300

    resolves #2

--- QRAlistGet/index.js
+++ QRAlistGet/index.js
@@ -1,9 +1,7 @@
 var fs = require('fs');
 var mysql = require('mysql');
 
-
 exports.handler = (event, context, callback) => {
-
 
     context.callbackWaitsForEmptyEventLoop = false;
     console.log('Received event:', JSON.stringify(event, null, 2));
@@ -12,46 +10,43 @@ exports.handler = (event, context, callback) => {
     var qra;
     var qra_res;
     var msg;
-    if (process.env.TEST) {
-        var test = {
-            "qra": "LU"
-        };
-        qra = test.qra;
-    }
-    else {
+    
+    if (event.qra) {
+        qra = event.qra;
+    } else {
         qra = event.params.querystring.qra;
     }
 
     //***********************************************************
     var conn = mysql.createConnection({
-        host      :  'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com' ,  // give your RDS endpoint  here
-        user      :  'sqso' ,  // Enter your  MySQL username
-        password  :  'parquepatricios' ,  // Enter your  MySQL password
-        database  :  'sqso'    // Enter your  MySQL database name.
+        host: 'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com', // give your RDS endpoint  here
+        user: 'sqso', // Enter your  MySQL username
+        password: 'parquepatricios', // Enter your  MySQL password
+        database: 'sqso' // Enter your  MySQL database name.
     });
-
-
 
     //QRA FOUND => Update QRA with ID Cognito
 
-
-    conn.query ( "SELECT qra, CONCAT(COALESCE(qra,''), ' ', COALESCE(firstname,''),' ', COALESCE(lastname,'')) AS name  FROM qras where qra LIKE ?", qra.toUpperCase() + '%',   function(error,info) {  // querying the database
+    conn.query("SELECT qra, CONCAT(COALESCE(qra,''), ' ', COALESCE(firstname,''),' ', COALESCE(l" +
+            "astname,'')) AS name, profilepic  FROM qras where qra LIKE ?",
+    qra.toUpperCase() + '%', function (error, info) { // querying the database
         if (error) {
             console.log(error.message);
             // context.done(null,event);
             conn.destroy();
-            msg = { "error": "1",
-                "message": "Could not get profile picture" };
+            msg = {
+                "error": "1",
+                "message": "Could not get profile picture"
+            };
             return context.fail(msg);
-        }
-        else if (info.length > 0) {
+        } else if (info.length > 0) {
             console.log("data updated");
             console.log(info);
             conn.destroy();
-            qra_res =  JSON.parse(JSON.stringify(info));
+            qra_res = JSON.parse(JSON.stringify(info));
             msg = {
                 "error": "0",
-                "message":  qra_res
+                "message": qra_res
             };
 
             context.succeed(msg);
@@ -60,12 +55,13 @@ exports.handler = (event, context, callback) => {
             conn.destroy();
             msg = {
                 "error": "0",
-                "message": { "qra": qra,
-                    "url": "empty"}
+                "message": {
+                    "qra": qra,
+                    "url": "empty"
+                }
             };
             return context.succeed(msg);
         }
     });
-
 
 };
