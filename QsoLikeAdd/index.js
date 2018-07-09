ef3cd4377b67dc81b026788931489a8dddcba5fa
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-07-09 14:40:57 -0300

    resolves #24

--- QsoLikeAdd/index.js
+++ QsoLikeAdd/index.js
@@ -1,130 +1,154 @@
-var fs = require('fs');
+
 var mysql = require('mysql');
-// var async = require('async');
 
-exports.handler = (event, context, callback) => {
 
+exports.handler = async(event, context, callback) => {
 
     context.callbackWaitsForEmptyEventLoop = false;
 
-    var Sub;
-    var idqra_owner;
-    var qsoLikes;
-    var qso;
-    var picked;
+    var sub;
+    var qso;    
+
     var response = {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
         },
-        body: {"error": null,
+        body: {
+            "error": null,
             "message": null
         }
     };
 
     // var count;
-    if (process.env.TEST) {
-        var test = {     "qso": "473"
-        };
-        qso = test.qso;
-
+    if (event.qso) {
+        qso = event.qso;        
+        sub = event.sub;
+    } else {
+        qso = event.qso;        
+        sub = event.context.sub;
     }
-    else {
-        qso = event.body.qso;
-
-    }
-
-    if (process.env.TEST){
-        Sub = "7bec5f23-6661-4ba2-baae-d1d4f0440038";
-    }else if (event.context.sub){
-        Sub = event.context.sub;
-    }
-    console.log("sub =", Sub);
-
 
     //***********************************************************
-    var conn = mysql.createConnection({
-        host      :  'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com' ,  // give your RDS endpoint  here
-        user      :  'sqso' ,  // Enter your  MySQL username
-        password  :  'parquepatricios' ,  // Enter your  MySQL password
-        database  :  'sqso'    // Enter your  MySQL database name.
+    var conn = await mysql.createConnection({
+        host: 'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com', // give your RDS endpoint  here
+        user: 'sqso', // Enter your  MySQL username
+        password: 'parquepatricios', // Enter your  MySQL password
+        database: 'sqso' // Enter your  MySQL database name.
     });
-
-    // GET QRA ID of OWNER
-    console.log("select QRA to get ID of Owner");
-    console.log(qso);
-    conn.query ( "SELECT qras.idqras from qras where qras.idcognito=?", Sub,   function(error,info) {
-        if (error) {
-            console.log("Error when selecting QRA");
-            console.log(error);
-            conn.destroy();
-            response.body.error = 400;
-            response.body.message = "Error: Error when selecting QRA";
-            // return context.fail( "Error: Error when selecting QRA");
-            return callback(null, response);
-        }
-        else if (info.length === 0){
+    try {
+        
+        let idqras_owner = await checkQraCognito(sub);
+        if (!idqras_owner) {
             console.log("User does not exist");
-            response.body.error = 400;
-            response.body.message = "Error: User does not exist";
             conn.destroy();
-            //return context.fail( "Error: User does not exist");
+            response.body.error = 1;
+            response.body.message = "User does not exist";
+            callback("User does not exist");
+            return context.fail(response);
+        }
+               
+        
+        let likes = await getLikes(qso);
+        let found = likes.find(o => o.idqra === idqras_owner)
+        if (found) {
+            console.log("already liked");
+            //like already exist => do not insert again
+            response.body.error = 400;
+            response.body.message = likes.length;
             return callback(null, response);
         }
-        else if (info.length > 0) {
+        
+        info = await insertLike(idqras_owner, qso);
+        if (info) {            
+            await UpdateLikesCounterInQso(qso);            
+                conn.destroy();
+                response.body.error = 0;
+                response.body.message = likes.length + 1;
+                console.log("new likes ");
+                return callback(null, response);
+            
+        }
 
-            idqra_owner = JSON.parse(JSON.stringify(info))[0].idqras;
-            console.log("qraowner " + idqra_owner);
-
-            conn.query('SELECT * from qsos_likes WHERE idqso = ?', qso, function (error, qsoLikes) {
-                console.log("error");
-                console.log(error);
-                console.log(qsoLikes);
-                if (error) {
-                    console.log("Error when SELECTING QSO LIKES");
-                    console.log(error.message);
-                    conn.destroy();
-                    response.body.error = 400;
-                    response.body.message = "Error when SELECTING QSO LIKES";
-                    //return context.fail( "Error when Insert QSO LIKES");
-                    return callback(null, response);
-                } else {
-                    picked = qsoLikes.find(o => o.idqra === idqra_owner);
-                    console.log("picked");
-                    console.log(picked);
-                    if (picked) {
-                        console.log("already liked" + qsoLikes.length);
-                        //like already exist => do not insert again
-                        response.body.error = 0;
-                        response.body.message = qsoLikes.length;
-                        return callback(null, response);
-                    } else {
-                        //insert new like
-                        console.log("idqso " + qso + "idqra " + idqra_owner );
-                        conn.query('INSERT INTO qsos_likes SET idqso = ?, idqra=?', [qso, idqra_owner], function (error, info) {
-                            if (error) {
-                                console.log("Error when Insert QSO LIKES");
-                                console.log(error.message);
-                                conn.destroy();
-                                response.body.error = 400;
-                                response.body.message = "Error when Insert QSO LIKES";
-                                //return context.fail( "Error when Insert QSO LIKES");
-                                return callback(null, response);
-                            } //End If
-                            if (info.insertId) {
-                                console.log("QSOLIKES inserted", info.insertId);
-                                conn.destroy();
-                                response.body.error = 0;
-                                response.body.message = qsoLikes.length + 1;
-                                console.log("new Like" + response.message);
-                                return callback(null, response);
-                            }
-                        }); //End Insert
+    } catch (e) {
+        console.log("Error executing QSO Likes Add");
+        console.log(e);
+        conn.destroy();
+        callback(e.message);
+        response.body.error = 1;
+        response.body.message = e.message;       
+        callback(null,response);
+        return context.fail(response);
+    }
+    function UpdateLikesCounterInQso(qso) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("UpdateLikesCounterInQso");
+            conn
+                .query('UPDATE sqso.qsos SET likes_counter = likes_counter+1  WHERE idqsos=?', qso, function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
                     }
-                }//End If
-            }); //End Select qsos_likes
-        } //ENDIF
-    }); //SELECT QSO TABLE WITH QSO and QRA
+                    resolve(JSON.parse(JSON.stringify(info)));
+                    // console.log(info);
+                });
+        });
+    }
+    function checkQraCognito(sub) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("checkQraCognito");
+            conn
+                .query("SELECT idqras FROM qras where idcognito=? LIMIT 1", sub, function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve(JSON.parse(JSON.stringify(info))[0].idqras);
+                });
+        });
+    }   
+    function getLikes(qso) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("checkQsoAlreadyLiked");
+            conn
+                .query("SELECT * from qsos_likes WHERE idqso = ?", [
+                    qso
+                ], function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve(JSON.parse(JSON.stringify(info)));
+                });
+        });
+    }
+    function insertLike(idqra_owner, qso) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("insertLike");
+            conn
+                .query("INSERT INTO qsos_likes SET idqso = ?, idqra=?", [
+                    qso, idqra_owner
+                ], function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve(JSON.parse(JSON.stringify(info)).insertId);
+                });
+        });
+    }
+   
 
 };
--- QsoLikeRemove/index.js
+++ QsoLikeRemove/index.js
@@ -1,23 +1,18 @@
-var fs = require('fs');
+
 var mysql = require('mysql');
-// var async = require('async');
 
-exports.handler = (event, context, callback) =>
-{
 
+exports.handler = async(event, context, callback) => {
 
     context.callbackWaitsForEmptyEventLoop = false;
 
-    var Sub;
-    var idqra_owner;
-    var qsoLikes;
-    var qso;
-    var picked;
+    var sub;
+    var qso;    
+
     var response = {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-            "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
         },
         body: {
@@ -27,113 +22,132 @@ exports.handler = (event, context, callback) =>
     };
 
     // var count;
-    if (process.env.TEST) {
-        var test = {
-            "qso": "473"
-        };
-        qso = test.qso;
-
+    if (event.qso) {
+        qso = event.qso;        
+        sub = event.sub;
+    } else {
+        qso = event.qso;        
+        sub = event.context.sub;
     }
-    else {
-        qso = event.body.qso;
-
-    }
-
-    if (process.env.TEST) {
-        Sub = "7bec5f23-6661-4ba2-baae-d1d4f0440038";
-    } else if (event.context.sub) {
-        Sub = event.context.sub;
-    }
-    console.log("sub =", Sub);
-
 
     //***********************************************************
-    var conn = mysql.createConnection({
-        host: 'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com',  // give your RDS endpoint  here
-        user: 'sqso',  // Enter your  MySQL username
-        password: 'parquepatricios',  // Enter your  MySQL password
-        database: 'sqso'    // Enter your  MySQL database name.
+    var conn = await mysql.createConnection({
+        host: 'sqso.clqrfqgg8s70.us-east-1.rds.amazonaws.com', // give your RDS endpoint  here
+        user: 'sqso', // Enter your  MySQL username
+        password: 'parquepatricios', // Enter your  MySQL password
+        database: 'sqso' // Enter your  MySQL database name.
     });
-
-    // GET QRA ID of OWNER
-    console.log("select QRA to get ID of Owner");
-    console.log(qso);
-    conn.query("SELECT qras.idqras from qras where qras.idcognito=?", Sub, function (error, info) {
-        if (error) {
-            console.log("Error when selecting QRA");
-            console.log(error);
-            conn.destroy();
-            response.body.error = 400;
-            response.body.message = "Error: Error when selecting QRA";
-            // return context.fail( "Error: Error when selecting QRA");
-            return callback(null, response);
-        }
-        else if (info.length === 0) {
+    try {
+        
+        let idqras_owner = await checkQraCognito(sub);
+        if (!idqras_owner) {
             console.log("User does not exist");
-            response.body.error = 400;
-            response.body.message = "Error: User does not exist";
             conn.destroy();
-            //return context.fail( "Error: User does not exist");
+            response.body.error = 1;
+            response.body.message = "User does not exist";
+            callback("User does not exist");
+            return context.fail(response);
+        }
+               
+        let likes = await getLikes(qso);
+        let found = likes.find(o => o.idqra === idqras_owner)
+        if (!found) {       
+            console.log("no like found");
+            //like already exist => do not insert again
+            response.body.error = 400;
+            response.body.message = likes.length;
             return callback(null, response);
         }
-        else if (info.length > 0) {
+        
+        info = await deleteLike(idqras_owner, qso);
+        if (info) {            
+            await UpdateLikesCounterInQso(qso);            
+                conn.destroy();
+                response.body.error = 0;
+                response.body.message = likes.length - 1;
+                console.log("likes deleted ");
+                return callback(null, response);
+            
+        }
 
-            idqra_owner = JSON.parse(JSON.stringify(info))[0].idqras;
-            console.log("qraowner " + idqra_owner);
-
-            conn.query('SELECT * from qsos_likes WHERE idqso = ?', qso, function (error, qsoLikes) {
-                console.log("error");
-                console.log(error);
-                console.log(qsoLikes);
-                if (error) {
-                    console.log("Error when SELECTING QSO LIKES");
-                    console.log(error.message);
-                    conn.destroy();
-                    response.body.error = 400;
-                    response.body.message = "Error when SELECTING QSO LIKES";
-                    //return context.fail( "Error when Insert QSO LIKES");
-                    return callback(null, response);
-                } else {
-                    picked = qsoLikes.find(o => o.idqra === idqra_owner);
-                    console.log("picked");
-                    console.log(picked);
-                    if (picked) {
-                        //remove like
-                        console.log("idqso " + qso + "idqra " + idqra_owner);
-                        conn.query('DELETE FROM qsos_likes where idqso=? and idqra=?', [qso, idqra_owner], function (error, info) {
-                            if (error) {
-                                console.log("Error when Deleting QSO LIKES");
-                                console.log(error.message);
-                                conn.destroy();
-                                response.body.error = 400;
-                                response.body.message = "Error when Deleting QSO LIKES";
-                                //return context.fail( "Error when Insert QSO LIKES");
-                                return callback(null, response);
-                            } else if (info.affectedRows > 0) {
-                                console.log("QSOLIKES deleted ", info.affectedRows);
-                                conn.destroy();
-                                response.body.error = 0;
-                                response.body.message = qsoLikes.length - 1;
-                                console.log("Like Removed" + response.body.message);
-                                return callback(null, response);
-                            }
-                        }); //End Delete
+    } catch (e) {
+        console.log("Error executing QSO Likes Delete");
+        console.log(e);
+        conn.destroy();
+        callback(e.message);
+        response.body.error = 1;
+        response.body.message = e.message;       
+        callback(null,response);
+        return context.fail(response);
+    }
+    function UpdateLikesCounterInQso(qso) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("UpdateLikesCounterInQso");
+            conn
+                .query('UPDATE sqso.qsos SET likes_counter = likes_counter-1  WHERE idqsos=?', qso, function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
                     }
-                    else {
-                        console.log("No Likes Found");
-                        console.log(error.message);
-                        conn.destroy();
-                        response.body.error = 400;
-                        response.body.message = "No Likes Found";
-                        //return context.fail( "Error when Insert QSO LIKES");
-                        return callback(null, response);
-
+                    resolve(JSON.parse(JSON.stringify(info)));
+                    // console.log(info);
+                });
+        });
+    }
+    function checkQraCognito(sub) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("checkQraCognito");
+            conn
+                .query("SELECT idqras FROM qras where idcognito=? LIMIT 1", sub, function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
                     }
 
-                }//End If
-            }); //End Select qsos_likes
-        } //ENDIF
-    }); //SELECT QSO TABLE WITH QSO and QRA
+                    resolve(JSON.parse(JSON.stringify(info))[0].idqras);
+                });
+        });
+    }   
+    function getLikes(qso) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("checkQsoAlreadyLiked");
+            conn
+                .query("SELECT * from qsos_likes WHERE idqso = ?", [
+                    qso
+                ], function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
+                    }
 
-}
-;
+                    resolve(JSON.parse(JSON.stringify(info)));
+                });
+        });
+    }
+    function deleteLike(idqra_owner, qso) {
+        return new Promise(function (resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            console.log("insertLike");
+            conn
+                .query('DELETE FROM qsos_likes where idqso=? and idqra=?', [
+                    qso, idqra_owner
+                ], function (err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    resolve(JSON.parse(JSON.stringify(info)).affectedRows);
+                });
+        });
+    }
+   
+
+};
