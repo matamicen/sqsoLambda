68f6cb7b49b8a5c8df273978a926d1af19280134
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2019-01-08 15:20:58 -0300

    resolves #126

---
+++ MonthlyJob/index.js
@@ -0,0 +1,73 @@
+var mysql = require('mysql');
+const warmer = require('lambda-warmer');
+
+exports.handler = async(event, context, callback) => {
+
+    if (await warmer(event))
+        return 'warmed';
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    var response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+        },
+        body: {
+            "error": null,
+            "message": null
+        }
+    };
+
+    //***********************************************************
+    if (!event['stage-variables']) {
+        console.log("Stage Variables Missing");
+        conn.destroy();
+        response.body.error = 1;
+        response.body.message = "Stage Variables Missing";
+        return callback(null, response);
+    }
+    var url = event['stage-variables'].url;
+    var conn = await mysql.createConnection({
+        host: event['stage-variables'].db_host, // give your RDS endpoint  here
+        user: event['stage-variables'].db_user, // Enter your  MySQL username
+        password: event['stage-variables'].db_password, // Enter your  MySQL password
+        database: event['stage-variables'].db_database // Enter your  MySQL database name.
+    });
+    try {
+        let info = await deleteCounters();
+        conn.destroy();
+        response.body.error = 0;
+        response.body.message = info;
+        console.log("new follower ");
+        return callback(null, response);
+
+    }
+    catch (e) {
+        console.log("Error executing Monthly Job");
+        console.log(e);
+        conn.destroy();
+
+        response.body.error = 1;
+        response.body.message = e.message;
+
+        return callback(null, response);
+    }
+    async function deleteCounters() {
+
+        return new Promise(function(resolve, reject) {
+            // The Promise constructor should catch any errors thrown on this tick.
+            // Alternately, try/catch and reject(err) on catch.
+            conn
+                .query("UPDATE qras SET monthly_links=0, monthly_scans=0", function(err, info) {
+                    // Call reject on error states, call resolve with results
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(JSON.parse(JSON.stringify(info)));
+                });
+        });
+
+    }
+
+};
