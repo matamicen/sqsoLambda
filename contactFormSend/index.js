6540b7c82aeb66a5b08c5901f58a907043eea93a
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2019-05-01 20:02:12 -0300

    #160

--- contactFormSend/index.js
+++ contactFormSend/index.js
@@ -1,44 +1,48 @@
-var AWS = require("aws-sdk");
-var ses = new AWS.SES();
+var AWS = require('aws-sdk')
+var ses = new AWS.SES()
 
-var RECEIVERS = ["lisandrolan@gmail.com"];
-var SENDER = "lisandrolan@gmail.com"; // make sure that the sender email is properly set up in your Amazon SES
-
+var RECEIVERS = ['lisandrolan@gmail.com'];
+var SENDER = 'lisandrolan@gmail.com'; // make sure that the sender email is properly set up in your Amazon SES
+var response = {
+    statusCode: 200,
+    headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+    },
+    body: {
+        "error": null,
+        "message": null
+    }
+};
 exports.handler = (event, context, callback) => {
-  console.log("Received event:", event);
-  sendEmail(event, function(err, data) {
-    var response = {
-      isBase64Encoded: false,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "http://example.com",
-      },
-      statusCode: 200,
-      body: '{"result": "Success."}',
-    };
-    callback(err, response);
-  });
+    console.log('Received event:', event);
+    sendEmail(event, function (err, data) {
+        response.body.error = 0;
+        response.body.message = err;
+        callback(null, response);
+
+    });
 };
 
 function sendEmail(event, done) {
-  var params = {
-    Destination: {
-      ToAddresses: RECEIVERS,
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data:
-            "\nEmail: " + event.body.email + "\nMessage: " + event.body.message,
-          Charset: "UTF-8",
+
+    var params = {
+        Destination: {
+            ToAddresses: RECEIVERS
         },
-      },
-      Subject: {
-        Data: "Contact Form inquiry: " + event.body.email,
-        Charset: "UTF-8",
-      },
-    },
-    Source: SENDER,
-  };
-  ses.sendEmail(params, done);
+        Message: {
+            Body: {
+                Text: {
+                    Data: '\nEmail: ' + event.body.email + '\nMessage: ' + event.body.message,
+                    Charset: 'UTF-8'
+                }
+            },
+            Subject: {
+                Data: 'Contact Form inquiry: ' + event.body.email,
+                Charset: 'UTF-8'
+            }
+        },
+        Source: SENDER
+    }
+    ses.sendEmail(params, done);
 }
