427075db85f6b3954138f24b9af426844969c72d
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2019-08-19 19:36:51 -0300

    #50

--- qraGetRecommendedFollowers/index.js
+++ qraGetRecommendedFollowers/index.js
@@ -3,7 +3,7 @@ const warmer = require("lambda-warmer");
 var AWS = require("aws-sdk");
 AWS.config.region = "us-east-1";
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async(event, context, callback) => {
   console.log(event);
   // if a warming event
   if (await warmer(event)) return "warmed";
@@ -56,12 +56,15 @@ exports.handler = async (event, context, callback) => {
     }
     let output = {};
     output.followingMe = await getFollowingMe(idqra_owner);
+    output.taggedMe = await getTaggedMe(idqra_owner);
+    output.topFollowed = await getTopFollowed(idqra_owner);
 
     conn.destroy();
     response.body.error = 0;
     response.body.message = output;
     context.succeed(response);
-  } catch (e) {
+  }
+  catch (e) {
     console.log("Error when select QRA");
     console.log(e);
     conn.destroy();
@@ -85,7 +88,8 @@ exports.handler = async (event, context, callback) => {
           }
           if (info.length > 0) {
             resolve(JSON.parse(JSON.stringify(info))[0].idqras);
-          } else {
+          }
+          else {
             resolve();
           }
         }
@@ -98,14 +102,68 @@ exports.handler = async (event, context, callback) => {
       // The Promise constructor should catch any errors thrown on this tick.
       // Alternately, try/catch and reject(err) on catch.
       conn.query(
-        "select fme.*, qras.* " +
-          " FROM sqso.qra_followers as fme " +
-          " left outer join  sqso.qra_followers as fing " +
-          " on fme.idqra = fing.idqra_followed " +
-          " and fme.idqra_followed = fing.idqra " +
-          " inner join qras on fme.idqra = qras.idqras" +
-          " where fme.idqra_followed = ? " +
-          " and fing.idqra is null",
+        "SELECT qras.idqras, qras.qra, qras.avatarpic, qras.firstname, qras.lastname,  followers_counter, qsos_counter, " +
+        "qras.comments_counter " +
+        " FROM sqso.qra_followers as fme " +
+        " left outer join  sqso.qra_followers as fing " +
+        " on fme.idqra = fing.idqra_followed " +
+        " and fme.idqra_followed = fing.idqra " +
+        " inner join qras on fme.idqra = qras.idqras" +
+        " where fme.idqra_followed = ? " +
+        " and fing.idqra is null",
+        idqra,
+        function(err, info) {
+          // Call reject on error states, call resolve with results
+          if (err) {
+            return reject(err);
+          }
+          resolve(JSON.parse(JSON.stringify(info)));
+        }
+      );
+    });
+  }
+
+  function getTaggedMe(idqra) {
+    return new Promise(function(resolve, reject) {
+      // The Promise constructor should catch any errors thrown on this tick.
+      // Alternately, try/catch and reject(err) on catch.
+      conn.query(
+        "SELECT qras.idqras, qras.qra, qras.avatarpic, qras.firstname, qras.lastname,  followers_counter, qsos_counter, " +
+        "qras.comments_counter " +
+        "from qsos as q " +
+        "inner join qsos_qras as qqr on " +
+        "q.idqsos = qqr.idqso " +
+        "left outer join  sqso.qra_followers as fing " +
+        "on q.idqra_owner = fing.idqra_followed  " +
+        "and qqr.idqra = fing.idqra " +
+        "inner join qras on q.idqra_owner = qras.idqras " +
+        "where qqr.idqra = ? " +
+        "and isOwner = 0 " +
+        "and fing.idqra is null " +
+        "group by q.idqra_owner",
+        idqra,
+        function(err, info) {
+          // Call reject on error states, call resolve with results
+          if (err) {
+            return reject(err);
+          }
+          resolve(JSON.parse(JSON.stringify(info)));
+        }
+      );
+    });
+  }
+
+  function getTopFollowed(idqra) {
+    return new Promise(function(resolve, reject) {
+      // The Promise constructor should catch any errors thrown on this tick.
+      // Alternately, try/catch and reject(err) on catch.
+      conn.query(
+        "SELECT qras.idqras, qras.qra, qras.avatarpic, qras.firstname, qras.lastname,  followers_counter, qsos_counter " +
+        "FROM sqso.qras " +
+        "where idqras <> ? and " +
+        "disabled = 0 " +
+        "order by   followers_counter desc " +
+        "limit 50",
         idqra,
         function(err, info) {
           // Call reject on error states, call resolve with results
